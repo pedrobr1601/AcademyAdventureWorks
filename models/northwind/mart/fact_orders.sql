2da89028eb3d68243550eb637dd255efa9a963a9
roeckerpedro@gmail.com <roeckerpedro@gmail.com>
2021-10-08 00:52:42 +0000

    commitou

--- models/northwind/mart/fact_orders.sql
+++ models/northwind/mart/fact_orders.sql
@@ -2,28 +2,73 @@
 
 with
     customers as (
-        select
-        customer_sk
-        , customer_id
+        select *
         from {{ref('dim_customers')}}
     )
-, orders_with_sk as (
+
+    , products as (
+        select *
+        from {{ref('dim_products')}}
+    )
+     , shippers as (
+        select *
+        from {{ref('dim_shippers')}}
+    )
+     , suppliers as (
+        select *
+        from {{ref('dim_suppliers')}}
+    )
+    , orders_with_sk as (
     select
-    public_orders.order_id
-    , customers.customer_id
-    , public_orders.order_date
-    , public_orders.ship_region
-    , public_orders.shipped_date
-    , public_orders.ship_country
-    , public_orders.ship_address
-    , public_orders.ship_postal_code
-    , public_orders.ship_city as cidade
-    , public_orders.ship_name
-    , public_orders.freight
-    , public_orders.required_date
+    orders.order_id
+    , customers.customer_id as customer_fk
+    , shippers.shipper_sk as shipper_fk
+    , orders.order_date
+    , orders.ship_region
+    , orders.shipped_date
+    , orders.ship_country
+    , orders.ship_address
+    , orders.ship_postal_code
+    , orders.ship_city
+    , orders.ship_name
+    , orders.freight
+    , orders.required_date
     
-    from {{ref('stg_orders')}} public_orders
-    left join customers customers on public_orders.customer_id = customers.customer_id
+    from {{ref('stg_orders')}} as orders
+    left join customers on orders.customer_id = customers.customer_id
+    left join shippers on orders.shipper_id = shippers.shipper_id
+)
+, order_detail_with_sk as (
+    select
+     order_id
+     , products.product_sk as product_fk
+     , order_detail.unit_price
+     , order_detail.quantity
+     , order_detail.discount
+    from {{ref('stg_order_detail')}} as order_detail
+    left join products on order_detail.product_id = products.product_id
+)
+, final as (
+    select
+    order_detail_with_sk.order_id
+    , orders_with_sk.customer_fk
+    , orders_with_sk.shipper_fk     
+    , orders_with_sk.order_date
+    , orders_with_sk.ship_region
+    , orders_with_sk.shipped_date
+    , orders_with_sk.ship_country
+    , orders_with_sk.ship_address
+    , orders_with_sk.ship_postal_code
+    , orders_with_sk.ship_city
+    , orders_with_sk.ship_name
+    , orders_with_sk.freight
+    , orders_with_sk.required_date
+    , order_detail_with_sk.product_fk   
+    , order_detail_with_sk.unit_price
+    , order_detail_with_sk.quantity
+    , order_detail_with_sk.discount
+    from orders_with_sk
+    left join order_detail_with_sk on orders_with_sk.order_id = order_detail_with_sk.order_id
 )
 select *
-from orders_with_sk
+from final
